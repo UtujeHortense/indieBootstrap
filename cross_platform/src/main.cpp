8c32ec68b5b63ce170885719b80c07bf96f38fd7
Hortense <utuje.hortense@epitech.eu>
2021-05-11 14:26:22 +0200

    [add] add mini project for testing

---
+++ cross_platform/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** hello
+** File description:
+** test
+*/
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "hello i finally executed" << std::endl;
+    return 0;
+}
